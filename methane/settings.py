2abf15e98ea80012841c19f3af1e7e1facc3cd69
Cxumhe <cxumhe@outlook.com>
2025-05-09 21:35:44 +0800

    fix: show avatar of JGFX

--- methane/settings.py
+++ methane/settings.py
@@ -78,13 +78,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db_methane',  # 数据库名称
         'USER': 'root',  # 账号
-        'PASSWORD': '',  # 密码
-        'HOST': 'localhost', #
-<<<<<<< HEAD
+        'PASSWORD': 'methane123',  # 密码
+        'HOST': 'mysql-db', #
         'PORT': '3306',
-=======
-        'PORT': '9080',
->>>>>>> fad851d (init proj.)
         'OPTIONS': {
             "init_command": "SET foreign_key_checks = 0;",
         }
@@ -119,7 +115,7 @@ DATETIME_FORMAT = 'Y-m-d H:i:s'
 TIME_ZONE = 'Asia/Shanghai'
 USE_TZ = False
 # 去掉默认Logo或换成自己Logo链接
-SIMPLEUI_LOGO = 'http://127.0.0.1:8000/media/jiguangfeixiang.jpg'
+SIMPLEUI_LOGO = '/media/jiguangfeixiang.jpg'
 
 USE_I18N = True
 
